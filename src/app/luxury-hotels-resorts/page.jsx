0bf5a7a141acb885eb842e166dddc3896caf36c2
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2025-03-24 01:39:00 +0530

    Update page.jsx

--- src/app/luxury-hotels-resorts/page.jsx
+++ src/app/luxury-hotels-resorts/page.jsx
@@ -1,23 +1,56 @@
+// import HeadingWithoutSwiper from '@component/components/headingWithoutSwiper'
+// import FilterSection from '@component/components/home/FilterSection'
+// import VideoContainer from '@component/components/home/VideoContainer'
+// import LuxuryHotelResort from '@component/components/luxaryHotelResort'
+// import SwiperComponent from '@component/components/SwiperComponent'
+// import React from 'react'
+
+// function page() {
+//   return (
+//     <>
+//     <section data-aos="zoom-in" className="hero-banner">
+//         <VideoContainer />
+//     </section>
+   
+
+    
+//       <LuxuryHotelResort />
+   
+//     </>
+//   )
+// }
+
+// export default page
+
 import HeadingWithoutSwiper from '@component/components/headingWithoutSwiper'
 import FilterSection from '@component/components/home/FilterSection'
 import VideoContainer from '@component/components/home/VideoContainer'
 import LuxuryHotelResort from '@component/components/luxaryHotelResort'
+import TransparentBackground from '@component/components/commonPage/TransparentBackground'
 import SwiperComponent from '@component/components/SwiperComponent'
+import "./page.css";
 import React from 'react'
 
-function page() {
+
+function Page() {
   return (
     <>
-    <section data-aos="zoom-in" className="hero-banner">
+      <section data-aos="zoom-in" className="hero-banner">
         <VideoContainer />
-    </section>
-   
+      </section>
 
+      <TransparentBackground />
+        <div className='filterSection' >
+        <FilterSection />
+        </div>
+        <LuxuryHotelResort />
     
-      <LuxuryHotelResort />
+ 
+   
+    
    
     </>
   )
 }
 
-export default page
+export default Page;
